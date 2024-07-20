a597074cbb62332c15e82e7db68ec2e504a485fd
sushant1424 <sushantshrestha184@gmail.com>
2024-07-20 19:25:24 +0545

    add pop up modal for coninement

--- admin/categories.php
+++ admin/categories.php
@@ -64,28 +64,48 @@ adminLogin();
     </div>
   </div>
 
-  <!-- Category Modal -->
-  <div class="modal fade" id="categories" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-    <div class="modal-dialog">
-      <form id="categories_form">
-        <div class="modal-content">
-          <div class="modal-header">
-            <h1 class="modal-title fs-5">Add Category</h1>
-          </div>
-          <div class="modal-body">
-            <div class="mb-3">
-              <label class="form-label">Category name: </label>
-              <input type="text" name="category_name" id="category_name_inp" class="form-control shadow-none" required>
-            </div>
-          </div>
-          <div class="modal-footer">
-            <button type="reset" class="btn text-secondary shadow-none" data-bs-dismiss="modal">Cancel</button>
-            <button type="submit" class="btn custom-bg">Add Category</button>
+ <!-- Category Modal -->
+<div class="modal fade" id="categories" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+  <div class="modal-dialog">
+    <form id="categories_form">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h1 class="modal-title fs-5">Add Category</h1>
+        </div>
+        <div class="modal-body">
+          <div class="mb-3">
+            <label class="form-label">Category name: </label>
+            <input type="text" name="category_name" id="category_name_inp" class="form-control shadow-none" required>
           </div>
         </div>
-      </form>
+        <div class="modal-footer">
+          <button type="reset" class="btn text-secondary shadow-none" data-bs-dismiss="modal">Cancel</button>
+          <button type="submit" class="btn custom-bg">Add Category</button>
+        </div>
+      </div>
+    </form>
+  </div>
+</div>
+
+<!-- Confirmation Modal -->
+<div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="confirmModalLabel">Confirm Your Decision</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Are you sure you want to delete this category?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button id="confirmDeleteButton" type="button" class="btn btn-danger">Delete</button>
+      </div>
     </div>
   </div>
+</div>
+
 
   <?php
   require('inc/script.php');
--- admin/users.php
+++ admin/users.php
@@ -66,6 +66,25 @@ adminLogin();
     </div>
   </div>
 
+  <!-- Confirmation Modal -->
+<div class="modal fade" id="confirmModal" tabindex="-1" aria-labelledby="confirmModalLabel" aria-hidden="true">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h5 class="modal-title" id="confirmModalLabel">Confirm Your Decision</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div class="modal-body">
+        Are you sure you want to delete this user?
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
+        <button id="confirmDeleteButton" type="button" class="btn btn-danger">Delete</button>
+      </div>
+    </div>
+  </div>
+</div>
+
  
 
 
